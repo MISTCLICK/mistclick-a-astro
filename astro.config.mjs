53402b8df60500e0af8143f17cf7a438b6ce4e27
Arina Vasiljeva <mistclick.a@gmail.com>
2023-09-26 22:10:02 +0300

    polished minor stuff.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@ import { defineConfig } from "astro/config";
 
 import prefetch from "@astrojs/prefetch";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [prefetch(), tailwind()],
+  integrations: [prefetch(), tailwind(), sitemap({
+    lastmod: new Date()
+  })],
+  site: 'https://mistclick.me',
   markdown: {
     shikiConfig: {
       theme: "min-light"
