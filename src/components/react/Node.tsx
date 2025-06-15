07da67fec3fcfcb4c36aa0840f20c7d334dd17c3
Arina Vasiljeva <mistclick.a@gmail.com>
2025-06-15 05:40:57 +0200

    a little fix to the animations

--- src/components/react/Node.tsx
+++ src/components/react/Node.tsx
@@ -2,6 +2,14 @@ import type { SparklesIcon } from "lucide-react";
 import type { ComponentProps } from "react";
 import { cn, useSectionContext } from "../../util";
 
+const insetList = [
+  "inset-x-[-45%_0]",
+  "inset-x-[-25%_0]",
+  "inset-x-[0_0]",
+  "inset-x-[25%_0]",
+  "inset-x-[45%_0]",
+];
+
 type NodeProps = {
   id: number;
   Icon: React.FC<ComponentProps<typeof SparklesIcon>>;
@@ -21,20 +29,15 @@ const Node: React.FC<NodeProps> = ({
     <section
       id="node"
       className={cn(
-        "group/node mb-6 transition-all duration-300 relative max-w-[1920px]",
+        "group/node mb-6 transition-all duration-300 max-w-[1920px]",
+        insetList[id],
         id !== selectedNode
           ? "animate-nimbus"
-          : "-translate-y-120 absolute bg-linear-to-br from-primary to-primary/60 animate-article-expand rounded-tl-3xl! rounded-md p-0! flex-row! justify-between! items-start!",
+          : "mx-auto bg-linear-to-br absolute from-primary to-primary/60 animate-article-expand rounded-tl-3xl! rounded-md p-0! flex-row! justify-between! items-start!",
         id === selectedNode
-          ? id === 1
-            ? "-translate-x-[25%]"
-            : id === 2
-            ? "-translate-x-[55%]"
-            : id === 3
-            ? "flex-row-reverse! -translate-x-[80%] bg-linear-to-bl rounded-tr-3xl! rounded-tl-md!"
+          ? id >= 3
+            ? "flex-row-reverse! bg-linear-to-bl rounded-tr-3xl! rounded-tl-md!"
             : id === 4
-            ? "flex-row-reverse! -translate-x-[100%] bg-linear-to-bl rounded-tr-3xl! rounded-tl-md!"
-            : ""
           : "",
         !Number.isNaN(selectedNode) && id !== selectedNode
           ? "blur-xs opacity-60"
